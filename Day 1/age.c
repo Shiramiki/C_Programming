64bb541ba0158acb4d0bc1676584997346242267
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-07-25 21:19:00 +0300

    Update

--- Day 1/age.c
+++ Day 1/age.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     /*
-        This progran allows the user to input their name, then outputs whether or not the are an adult
+        This program allows the user to input their age, then outputs whether or not they are an adult.
     */
     int age;
 
-    printf("Please enter your age");
+    printf("Please enter your age: ");
     scanf("%d", &age);
 
-    if (age<18){
-        printf("You are a minor");
+    if (age < 18) {
+        printf("You are a minor\n");
+    } else {
+        printf("You are an adult\n");
     }
 
-    else{
-        printf("You are an adult");
-    }
+    return 0;   // Return 0 to indicate successful completion
 }
