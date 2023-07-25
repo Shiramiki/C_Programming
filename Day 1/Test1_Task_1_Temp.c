e0d301f99ea917e04a38c8c2c30c5b2c233437e0
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-07-25 21:13:01 +0300

    Change to Test1

--- Day 1/Test1_Task_1_Temp.c
+++ Day 1/Test1_Task_1_Temp.c
@@ -1,13 +1,14 @@
-#include <stdio.h> //missing stdlib
+#include <stdio.h>
 
-int main (){
+int main() {
     //this program changes temperature from degree Celsius to Fahrenheit
     float temp_c, temp_f;        
-    printf("Please enter temperature in Degrees Celsius");
+    printf("Please enter temperature in Degrees Celsius: ");
     scanf("%f", &temp_c);
 
-    temp_f=((9/5) * temp_c + 32);   //a float cannot be equal to an int
+    temp_f = ((9.0/5.0) * temp_c + 32);   // Using floating point values for the conversion
 
-    printf("\n\n The Temperature in Fahrenheit is %.2f", temp_f);
+    printf("\n\nThe Temperature in Fahrenheit is %.2f", temp_f);
+
+    return 0;   // Return 0 to indicate successful completion
 }
-
